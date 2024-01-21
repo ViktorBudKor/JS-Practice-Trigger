78346be75f6b27197ff5d296f3b5a20d11c01009
ViktorBudKor <kitkat_115@mail.ru>
2024-01-21 16:53:48 +0300

    Практика 5

---
+++ Practice_5.js
@@ -0,0 +1,4 @@
+var arr = [31, 10, 'chicken', 9, 'fish', 10];
+console.log(arr);
+console.log(arr.filter(item => typeof item === "number"));
+console.log(arr.filter(item => typeof item === "string"));
