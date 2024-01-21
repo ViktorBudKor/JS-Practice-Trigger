ca1331634309dd93243b7ac2d0df4d7bef00eef4
ViktorBudKor <kitkat_115@mail.ru>
2024-01-21 16:29:00 +0300

    Практика 4 доработал

--- Practice_4.js
+++ Practice_4.js
@@ -1,6 +1,8 @@
 var massive = ["cat", "dog", "parrot", "horse", "fish", "chicken", "lion"];
 var array = [""];
 console.log(massive);
+
+
 do{
 array.push(massive[massive.length - 1]);
 massive.pop();
@@ -11,5 +13,14 @@ massive.push(array[array.length - 1]);
 array.pop();
 }
 console.log(massive);
+
+
+massive =["cat", "dog", "parrot", "horse", "fish", "chicken", "lion"];
 massive.shift();
+console.log(massive);
+
+
+massive = ["cat", "dog", "parrot", "horse", "fish", "chicken", "lion"];
+massive.pop();
+massive.pop();
 console.log(massive);
