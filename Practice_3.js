b5fd3dc07f379f06bba8bb10f4760833d7728db6
ViktorBudKor <kitkat_115@mail.ru>
2024-01-21 15:59:23 +0300

    Практика 3

---
+++ Practice_3.js
@@ -0,0 +1,8 @@
+var massive = ["cat", "dog", "parrot", "horse"];
+var index;
+for(let i = 0;i < massive.length;i++){
+    if (massive[i] == "parrot"){
+        index = i;
+    }
+}
+console.log(index);
